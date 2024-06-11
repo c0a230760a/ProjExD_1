a2467ccae6828c27f1141a781e992ea11ee7f927
shuto <c0a230760a@edu.teu.ac.jp>
2024-06-11 17:34:37 +0900

    演習3:ファイル名の変更

---
+++ flying_kokaton.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    kk_img = pg.transform.flip(kk_img, True, False) #kkimgを（左右、上下）で反転
+    kk_rct = kk_img.get_rect() #こうかとんのレクトの抽出
+    kk_rct.center = 300, 200
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        x = tmr%4800
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0])
+
+        
+        a = -1
+        b = 0
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            a = 0
+            b = -1
+        if key_lst[pg.K_DOWN]:
+            a = 0
+            b = 1
+        if key_lst[pg.K_LEFT]:
+            a = -1
+        if key_lst[pg.K_RIGHT]:
+            a = 1
+
+        kk_rct.move_ip((a, b)) #左に動かす
+
+
+        screen.blit(kk_img, kk_rct) #kkimgをkkrctに従ってスクリーンに張り付ける
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200) #FPS
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
